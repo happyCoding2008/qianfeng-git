046eccb307592b2bd8f7eb1b60edcddd621bc81d
happycodingnow <happycodingnow@sina.com>
2018-09-19 11:36:54 +0800

    修复bug

--- src/main/java/com/qianfeng/qianfenggit/controller/GitController.java
+++ src/main/java/com/qianfeng/qianfenggit/controller/GitController.java
@@ -12,6 +12,12 @@ public class GitController {
         System.out.println("生活如此美好");
         System.out.println("生活如此美好");
         System.out.println("2018....");
+        /*try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        System.out.println("修复某个性能问题");
         return "hello";
     }
 }
